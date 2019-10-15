8697dc262142d2e85163ddadb20979a57d7f0627
alejandro romero <aleja@DESKTOP-U2Q2U28>
2019-10-15 20:24:23 +0200

    ej4

--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Ejer3LinqPractice_SecondTry
 {
@@ -10,6 +12,17 @@ namespace Ejer3LinqPractice_SecondTry
     {
         static void Main(string[] args)
         {
+            String archivo = File.ReadAllText("Cars.json");
+            List<Car> cochesito = JsonConvert.DeserializeObject<List<Car>>(archivo);
+
+        }
+        public static string Ej4(List<Car> cochesio)
+        {
+            var colorCar = cochesio.Where(x => x.color == "Fuscia");
+            foreach (var a in colorCar)
+            {
+                Console.WriteLine("Marca: "+a.maker+" Modelo: "+a.model);
+            }
         }
     }
 }
